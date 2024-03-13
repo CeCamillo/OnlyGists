812082d34d83cb58fd06980e7d973e0300d24a82
CeCamillo <cesarcamillo2212@gmail.com>
2024-03-12 22:44:27 -0300

    Add Landing Page

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <h1 class="font-[Inter] text-red-500">OnlyGists</h1>
-    <Button label="Submit"/>
-  </div>
+  <NuxtLayout>
+    <NuxtPage></NuxtPage>
+  </NuxtLayout>
 </template>
---
+++ components/Logo/Logo.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex items-center gap-1 select-none">
+    <span class="text-2xl">Only</span>
+    <span class="font-[Inter] font-bold text-gray-700 text-2xl">Gists</span>
+  </div>
+</template>
