d657c76251b8935caad58a0c7603e20b66f92091
CeCamillo <cesarcamillo2212@gmail.com>
2024-03-13 12:35:48 -0300

    feat: add public profile screen

--- modules/auth/screens/Login/Login.vue
+++ modules/auth/screens/Login/Login.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SocialForm from '../../components/SocialForm/SocialForm.vue'
+
+const loading = ref<boolean>(false)
+
+const handleGithubLogin = async () => {
+  loading.value = true
+  // TODO Call API
+}
+</script>
 
 <template>
   <section class="w-full h-screen flex flex-col items-center justify-center gap-5 bg-gray-50">
     <Logo />
+    <SocialForm :loading="loading" @wants-login-with-github="handleGithubLogin" />
   </section>
 </template>
