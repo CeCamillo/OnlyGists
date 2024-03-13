d962e944e773d2588f3b8ad0f253b16c32988be3
CeCamillo <cesarcamillo2212@gmail.com>
2024-03-12 22:47:38 -0300

    Add SocialForm for Login

---
+++ modules/auth/screens/Login/Login.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="w-full h-screen flex flex-col items-center justify-center gap-5 bg-gray-50">
+    <Logo />
+  </section>
+</template>
