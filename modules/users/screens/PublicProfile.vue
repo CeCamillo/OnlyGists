8e1c62578327c3b30bdde4b4ddff04dc171707a8
CeCamillo <cesarcamillo2212@gmail.com>
2024-03-14 12:42:17 -0300

    feat: add widget group with condensed widgets to PublicProfile.vue

--- modules/users/screens/PublicProfile.vue
+++ modules/users/screens/PublicProfile.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import PublicHeadline from '../components/PublicHeadline/PublicHeadline.vue'
+import WidgetGroup from '../components/Widget/Group/Group.vue'
+import WidgetGroupLoader from '../components/Widget/Group/Loader.vue'
+import WidgetCondensed from '../components/Widget/Condensed/Condensed.vue'
 </script>
 
 <template>
   <PublicHeadline />
+
+  <WidgetGroup>
+    <WidgetGroupLoader :loading="true" :amount="3">
+      <WidgetCondensed :value="10" label="Gists no total" />
+      <WidgetCondensed :value="5" label="Gists gratuitos" />
+      <WidgetCondensed :value="5" label="Gists pagos" />
+    </WidgetGroupLoader>
+  </WidgetGroup>
 </template>
